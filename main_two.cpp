658129a0ae46aabf62734fdc8534cda898622846
Irene Koner <ireneckoner@gmail.com>
2026-03-07 23:19:01 +0530

    Add files via upload

---
+++ main_two.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      int a, b;
+      cout << "Enter first number: ";
+      cin >> a;
+
+      cout << "Enter second number: ";
+      cin >> b;
+
+      if (a<b)
+        cout << "a is less than b." << endl;
+      else
+        cout << "a is greater than b." << endl;
+
+        return 0;
+
+} 
