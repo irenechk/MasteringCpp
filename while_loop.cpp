63746fb8406bdc925267da8168d9c1dffb6313e6
Irene Koner <ireneckoner@gmail.com>
2026-04-12 23:03:00 +0530

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,19 @@
+//print numbers from 1 to n using while loop
+
+/*WHILE LOOP SYNTAX:
+while (condition) {
+    // operations
+    //step
+}*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int i = 1;
+    while (i <= 5) {
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
